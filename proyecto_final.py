a007120c996eef94f5cf498297107238abcdacfd
Gabriel Meza <89315623+StandbyG@users.noreply.github.com>
2025-07-03 21:13:35 -0500

    Update proyecto_final.py

--- proyecto_final.py
+++ proyecto_final.py
@@ -12,6 +12,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from IPython.display import display
 
 # Preprocesamiento y modelado
 from sklearn.model_selection import train_test_split, GridSearchCV
